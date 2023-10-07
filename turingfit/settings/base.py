20cbf35da7fbfcb1105d04493a32343cfbb58498
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-10-07 05:36:13 +0000

    Fix authentication issue

--- turingfit/settings/base.py
+++ turingfit/settings/base.py
@@ -77,19 +77,6 @@ AUTHENTICATION_BACKENDS = (
 SOCIAL_AUTH_STRAVA_KEY = env["STRAVA_KEY"]
 SOCIAL_AUTH_STRAVA_SECRET = env["STRAVA_SECRET"]
 SOCIAL_AUTH_STRAVA_SCOPE = ["activity:read_all"]
-SOCIAL_AUTH_PIPELINE = (
-    "social_core.pipeline.social_auth.social_details",
-    "social_core.pipeline.social_auth.social_uid",
-    "social_core.pipeline.social_auth.auth_allowed",
-    "social_core.pipeline.social_auth.social_user",
-    "social_core.pipeline.user.get_username",
-    "social_core.pipeline.social_auth.associate_by_email",  # <--- enable this one
-    "social_core.pipeline.user.create_user",
-    "social_core.pipeline.social_auth.associate_user",
-    "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
-    "core.auth.save_strava_auth",
-)
 
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
